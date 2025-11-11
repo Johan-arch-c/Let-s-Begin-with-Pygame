d84546d5bdbf2cf7f7b04a0155a40414218d19de
Johan-arch-c <johannithya@gmail.com>
2025-11-11 15:34:13 +1100

    hw

---
+++ hw.py
@@ -0,0 +1,20 @@
+import pygame
+import sys
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My First Game Screen")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
